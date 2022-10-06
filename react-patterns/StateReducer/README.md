15f0af0f7cb5ee6c71d69efa4494f5b3c046212d
ElhamFadel <elham2000fadel@gmail.com>
2022-10-06 10:55:38 +0300

    (React-pattern) Remove state reduce pattern cuaz definitly i like it

--- react-patterns/StateReducer/README.md
+++
@@ -1,2 +0,0 @@
-# State Intalization pattern
-make it easy for consumer of your custom hook to set the value of state
--- react-patterns/StateReducer/src/assets/index.js
+++
@@ -1,3 +0,0 @@
-export {default as image1} from "./1.jpeg";
-export {default as image2} from "./2.jpeg";
-export {default as image3} from "./3.jpeg";
